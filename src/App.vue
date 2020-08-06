772a0ea41d74728acb70c2f8294a387ee9a3dd7a
kerpadw09 <kerpadw09@gmail.com>
2020-08-06 15:34:54 +0800

    Remove footer

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,18 @@
 <template>
   <div>
-    <!-- HEADER START -->
     <Header />
-    <!-- HEADER END -->
-    <!-- MAIN APP START -->
     <div id="app">
       <router-view/>
     </div>
-    <!-- MAIN APP END -->
-    <!-- FOOTER START -->
-    <Footer />
-    <!-- FOOTER END -->
   </div>
 </template>
 
 <script>
 import Header from '@/components/Globals/Header'
-import Footer from '@/components/Globals/Footer'
 export default {
   name: 'App',
   components: {
-    Header,
-    Footer
+    Header
   }
 }
 </script>
